ff11cfd451c816456e7630b2e3df562bc8ac6ad4
Gumbers <gustavomarinsuppi@gmail.com>
2020-01-06 17:20:39 -0300

    feat: First commit of WebAdvert.Web

---
+++ WebAdvert.Web/Models/Accounts/SignupModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+    public class SignupModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public String Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(8, ErrorMessage = "Password must be at least eight characters long, have letter, have numbers and special characters!")]
+        [Display(Name ="Password")]
+        public String Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage ="Password and its confirmation do not match")]
+        public String ConfitmPassword { get; set; }
+
+    }
+}
