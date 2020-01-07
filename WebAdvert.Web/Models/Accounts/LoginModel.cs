182c30cdd03f8ddce8f059e6e41ff832ed5a689a
Gumbers <gustavomarinsuppi@gmail.com>
2020-01-06 23:43:23 -0300

    feat: adding login.cshtml template

--- WebAdvert.Web/Models/Accounts/LoginModel.cs
+++ WebAdvert.Web/Models/Accounts/LoginModel.cs
@@ -1,11 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebAdvert.Web.Models.Accounts
 {
     public class LoginModel
     {
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress]
+        [Display(Name ="Email")]
+        public String  Email { get; set; }
+
+        [Required(ErrorMessage ="Password is required")]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
     }
 }
